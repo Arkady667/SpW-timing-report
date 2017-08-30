2b2f6365ebfcff812202e03bbc7e995d71f53222
Arkady667 <arkadiusz.czuba@gmail.com>
2017-08-30 14:13:46 +0200

    Commit #2
    v0.1 - spw_test.tcl - working User Set adding inspite of error message

--- spw_test.tcl
+++ spw_test.tcl
@@ -11,42 +11,41 @@ set argv1 [lindex $::argv 1]
 set argv2 [lindex $::argv 2]
 set argv3 [lindex $::argv 3]
 
-# puts "$::argv0 is argv0"
-# puts "$::argv1 is argv1"
-# puts "$::argv2 is argv2"
+set setName { iSpwToReg }
+set setSourceStb { iSpw0Stb }
+set setSourceDat { iSpw0Dat }
+set setSink { *r.do* }
+puts $setName
 
-set designFile {C:\Users\aczuba\TCL\spw_script\P3_DPU.adb} 
+
+set designFileTest {C:\Users\aczuba\TCL\spw_script\P3_DPU.adb} 
+set designFile {C:\DPU FPGA repo v2\alllib\designs\P3CCB_RT\P3_DPU.adb}
 puts $designFile
 
-open_design $designFile
-	if  { [catch { open_design $designFile }]} { 
+open_design $designFileTest
+	if  { [catch { open_design $designFileTest }]} { 
 		puts "Failed to open design"
 		# Handle Failure 
 	} else { 
 		puts "Design opened successfully"
 		# Proceed to further processing 
 	} 
-
-puts "$::argv0 is argv0"
-puts "$::argv1 is argv1"
-puts "$::argv2 is argv2"
-	# set createSet "-name { iToRegClk } -source { $::argv1 } -sink { $::argv2 }"
-	st_create_set -name { iToRegClk } -source { $argv1 } -sink { $argv2 }
-
-	if  { [catch { st_create_set -name { iToRegClk } -source { $argv1 } -sink { $argv2 }}]} { 
+# st_create_set -name { iSpwToReg } -source { iSpw0* } -sink  { *r.do* }  
+st_create_set -name  $setName -source  $setSourceStb -source $setSourceDat -sink  $setSink  
+	if  { [catch {st_create_set -name  $setName -source  $setSourceStb -source $setSourceDat -sink  $setSink }]} {
 		puts "Failed to set path \n $::errorInfo \n $::errorCode"
 		# Handle Failure 
 	} else { 
 		puts "Path created successfully "
 		# Proceed to further processing 
 	} 
+st_commit
+
+# st_list_paths -set { iToRegClk }
 
 
-
-
-
-
-st_list_paths -set { iToRegClk }
-
+puts "$::argv0 is argv0"
+puts "$::argv1 is argv1"
+puts "$::argv2 is argv2"
 save_design {P3_DPU.adb}
 close_design
